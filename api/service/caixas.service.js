8a740b1d650269926ca335214c4cbff84fa266be
Suelen dos Santos Batista <suelen.batista@al.infnet.edu.br>
2021-05-04 16:08:52 -0300

    ajuste rota getBoxById

--- api/service/caixas.service.js
+++ api/service/caixas.service.js
@@ -7,8 +7,6 @@ const searchBoxByName = async (name) => {
   return resultFromDB ? true : false;
 };
 
-
-
 const findAllBoxes = async (id) => {
   const resultFromDB = await caixas.findAll({
     include:[{
@@ -18,17 +16,15 @@ const findAllBoxes = async (id) => {
   });
 
   return resultFromDB.map(item => {
-    const { id, name, description, price, assinantes } = item
-    
-        return{
-          id,
-          name,
-          description,
-          price,
-          qtd_subs:assinantes.length,
-        };
-  })
-
+    const { id, name, description, price, assinantes } = item;
+      return{
+        id,
+        name,
+        description,
+        price,
+        qtd_subs:assinantes.length,
+      };
+  });
 };
 
 const findBoxByIdNoAuth = async (id) =>{
@@ -52,45 +48,100 @@ const findBoxByUserProfile = async (id, userId, userType) => {
 
   let subscriptionFilter = resultFromDB.assinantes;
 
-  if (Number(userType) === 1) {
+  if (Number(userType) === 2) {
+    subscriptionFilter = subscriptionFilter.filter(item => item.id_user === userId);
+  }
 
-    const subscriptionDetail = subscriptionFilter.map((itemSubscription) => {
-      return {
-        id: itemSubscription.id,
-        member: {
-          id: itemSubscription.user.id,
-          name: itemSubscription.user.name,
-          email:itemSubscription.user.email,
-          birth_date: itemSubscription.user.birth_date,
-        },
-      };
-    });
+  const subscriptionDetail = subscriptionFilter.map((itemSubscription) => {
+    return {
+      id: itemSubscription.id,
+      member: {
+        id: itemSubscription.user.id,
+        name: itemSubscription.user.name,
+        email:itemSubscription.user.email,
+        birth_date: itemSubscription.user.birth_date,
+      },
+    };
+  });
     
-    return {
-      id: resultFromDB.id,
-      name: resultFromDB.name,
-      description: resultFromDB.description,
-      price: resultFromDB.price,
-      qtd_subs: subscriptionFilter.length,
-      assinantes: subscriptionDetail,
-    };
-
-  } else {
-    const memberDetail = subscriptionFilter.filter((itemFilter) => 
-      (Number(itemFilter.id_user) === Number(userId)));
-
-    const subsId = memberDetail[0].dataValues.id;
-
-    return {
-      id: resultFromDB.id,
-      name: resultFromDB.name,
-      description: resultFromDB.description,
-      price: resultFromDB.price,
-      member: memberDetail.length ? true : false,
-      subs_id:subsId,
-    };
+  return {
+    id: resultFromDB.id,
+    name: resultFromDB.name,
+    description: resultFromDB.description,
+    price: resultFromDB.price,
+    qtd_subs: subscriptionFilter.length,
+    assinantes: subscriptionDetail,
   };
 };
+
+// const findBoxByUserProfile = async (id, userId, userType) => {
+//   const resultFromDB = await caixas.findOne({
+//     where: { id: id },
+//     include:[{
+//       model: userCaixas,
+//       as: 'assinantes',
+//       include: [{
+//         model: users,
+//         as: 'user',
+//       }],
+//     }],
+//   });
+
+//   let subscriptionFilter = resultFromDB.assinantes;
+//   let result = []
+
+//   if (Number(userType) === 2) {
+
+//     if (Number(usuarioTipo) === 2) {
+//       subscriptionFilter = subscriptionFilter.filter(item => item.id_user === userId);
+//     }
+  
+
+
+//     const memberDetail = subscriptionFilter.filter((itemFilter) => 
+//       (Number(itemFilter.id_user) === Number(userId)));
+//     var subsId = memberDetail[0].dataValues.id;
+
+//     if(memberDetail){
+
+//     }
+
+
+//       console.log("##############################")
+//       console.log(item)
+
+//     return {
+//       id: resultFromDB.id,
+//       name: resultFromDB.name,
+//       description: resultFromDB.description,
+//       price: resultFromDB.price,
+//       // member: memberDetail.length ? true : false,
+//       // subs_id:subsId,
+//     };
+
+//   } else {
+//     const subscriptionDetail = subscriptionFilter.map((itemSubscription) => {
+//       return {
+//         id: itemSubscription.id,
+//         member: {
+//           id: itemSubscription.user.id,
+//           name: itemSubscription.user.name,
+//           email:itemSubscription.user.email,
+//           birth_date: itemSubscription.user.birth_date,
+//         },
+//       };
+//     });
+    
+//     return {
+//       id: resultFromDB.id,
+//       name: resultFromDB.name,
+//       description: resultFromDB.description,
+//       price: resultFromDB.price,
+//       qtd_subs: subscriptionFilter.length,
+//       assinantes: subscriptionDetail,
+//     };
+//   };
+// };
 
 const createNewBox = async (body) => {
   return await caixas.create({
